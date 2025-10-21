91aab4f292d77dd504b2acd1b4fce342094f15bd
Dev Account <Test@Mikes-MacBook-Pro.local>
2025-10-21 12:23:16 -0400

    Added the Offical OCAML resource for Patterns

---
+++ ocaml_projects/pattern_matching.ml
@@ -0,0 +1,12 @@
+let describe_number n =
+  match n with
+  | 0 -> "Zero"
+  | 1 -> "One"
+  | 2 -> "Two"
+  | _ -> "Other";;
+
+let () =
+  print_endline (describe_number 0);
+  print_endline (describe_number 9)
+
+#https://ocaml.org/manual/5.4/patterns.html
