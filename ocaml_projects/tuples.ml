212f39b3c565ab0bd87212cd44be852640c4d08e
Dev Account <Test@Mikes-MacBook-Pro.local>
2025-10-21 12:31:47 -0400

    Added the Offical resource for OCAML tuples

--- ocaml_projects/tuples.ml
+++ ocaml_projects/tuples.ml
@@ -2,3 +2,5 @@ let trade = ("AAPL", 185.42, 1500);;
 let (symbol, price, volume) = trade;;
 
 Printf.printf "Symbol: %s | Price: %.2f | Volume: %d\n" symbol price volume;;
+
+(*https://ocaml.org/manual/5.4/types.html#sec:tuple*)
